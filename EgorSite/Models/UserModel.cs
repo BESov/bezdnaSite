cd2a53270872e19e2d2fbfb06e5cbe7bd489c826
Egor <belikes@yandex.ru>
2013-11-24 21:05:42 +0400

    UserModel

---
+++ EgorSite/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EgorSite.Models
+{
+    public class UserModel
+    {
+        private string connStr = "";
+
+        public string[] GetUserNames()
+        {
+
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            SqlCommand command = conn.CreateCommand();
+            command.CommandText = "Select email From User";
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<string> emails = new List<string>();
+
+            while (reader.Read())
+            {
+                emails.Add(readers            }
+        }
+    }
+}
